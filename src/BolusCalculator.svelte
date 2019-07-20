de261ffd116489f2b3db8f1a13b9c04898c62b7e
root <root@Franois-PCTour.localdomain>
2019-07-21 00:55:48 +0200

    :zap: Add constraints

--- src/BolusCalculator.svelte
+++ src/BolusCalculator.svelte
@@ -1,6 +1,6 @@
 <script>
     let glycemie = 100; // mg/dl
-    let glucides; // g
+    let glucides = 0; // g
     let facteurCorrection = 45; // Facteur de correction mg/dl/U
     let objectif = 150; // Objectif glycémique mg/dl
     let rapportInsulineGlucides = 10; // g / U
@@ -9,7 +9,7 @@
 
     function calculeCorrection() {
         correction = glycemie > objectif 
-            ? (glycemie - objectif) / facteurCorrection 
+            ? ((glycemie - objectif) / facteurCorrection).toFixed(2)
             : 0;
     }
 
@@ -26,50 +26,50 @@
 
 <span>
     <label for="glycemie">Glycémie</label>
-    <input type="number" bind:value={glycemie} on:change={calculeCorrection} id="glycemie">
+    <input type="number" min="40" max="500" bind:value={glycemie} on:change={calculeCorrection} id="glycemie">
     <span>mg/dl</span>
 </span>
 
 <span>
     <label for="glucides">Glucides</label>
-    <input type="number" bind:value={glucides} on:change={calculeRepas} id="glucides">
+    <input type="number" min="0" max="100" bind:value={glucides} on:change={calculeRepas} id="glucides">
     <span>g</span>
 </span>
 
 <span>
     <label for="correction">Correction</label>
-    <input type="number" value={correction} id="correction">
+    <input type="number" value={correction} readonly step="0.01" id="correction">
     <span>U</span>
 </span>
 
 <span>
     <label for="repas">Bolus repas</label>
-    <input type="number" value={repas} id="repas">
+    <input type="number" value={repas} readonly step="0.01" id="repas">
     <span>U</span>
 </span>
 
 <span>
     <label for="bolus">Bolus total</label>
-    <input type="number" value={correction + repas} id="bolus">
+    <input type="number" value={Number(correction + repas).toFixed(2)} readonly step="0.01" id="bolus">
     <span>U</span>
 </span>
 
 <div class="settings">
     <span>
         <label for="facteurCorrection">Facteur de correction</label>
-        <input type="number" bind:value={facteurCorrection} on:change={calculeCorrection} id="facteurCorrection">
+        <input type="number" min="1" max="200" bind:value={facteurCorrection} on:change={calculeCorrection} id="facteurCorrection">
         <span>mg/dl/U</span>
     </span>
 
     <span>
         <label for="objectif">Objectif glycémique</label>
-        <input type="number" bind:value={objectif} on:change={calculeCorrection} id ="objectif">
+        <input type="number" min="80" max="200" bind:value={objectif} on:change={calculeCorrection} id ="objectif">
         <span>mg/dl</span>
     </span>
 
     <span>
         <label for="rapport">Rapport glucides / insuline</label>
-        <input type="number" bind:value={rapportInsulineGlucides} on:change={calculeRepas} id="rapport">
+        <input type="number" min="1" max="100" bind:value={rapportInsulineGlucides} on:change={calculeRepas} id="rapport">
         <span>g/U</span>
     </span>
 </div>
