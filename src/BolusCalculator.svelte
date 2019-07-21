1b173988b13aadec948b1e6e2f37b5000b681e7f
root <root@Franois-PCTour.localdomain>
2019-07-21 09:03:03 +0200

    :hammer: Use reactive declarations

--- src/BolusCalculator.svelte
+++ src/BolusCalculator.svelte
@@ -1,20 +1,14 @@
 <script>
     import Bolus from './Bolus.svelte';
+    import Setting from './Setting.svelte';
     let glycemie = 150; // mg/dl
     let glucides = 0; // g
     let facteurCorrection = 45; // Facteur de correction mg/dl/U
     let objectif = 150; // Objectif glycémique mg/dl
     let rapportInsulineGlucides = 10; // g / U
-    let correction = 0; // U
-    let repas = 0; // U
 
-    function calculeCorrection() {
-        correction = ((glycemie - objectif) / facteurCorrection);
-    }
-
-    function calculeRepas() {
-        repas = glucides / rapportInsulineGlucides;
-    }
+    $: correction = (glycemie - objectif) / facteurCorrection; // U
+    $: repas = glucides / rapportInsulineGlucides; // U
 </script>
 
 <style>
@@ -25,13 +19,13 @@
 
 <span>
     <label for="glycemie">Glycémie</label>
-    <input type="number" min="40" max="500" bind:value={glycemie} on:change={calculeCorrection} id="glycemie">
+    <input type="number" min="40" max="500" bind:value={glycemie} id="glycemie">
     <span>mg/dl</span>
 </span>
 
 <span>
     <label for="glucides">Glucides</label>
-    <input type="number" min="0" max="100" bind:value={glucides} on:change={calculeRepas} id="glucides">
+    <input type="number" min="0" max="100" bind:value={glucides} id="glucides">
     <span>g</span>
 </span>
 
@@ -51,21 +45,23 @@
 />
 
 <div class="settings">
-    <span>
-        <label for="facteurCorrection">Facteur de correction</label>
-        <input type="number" min="1" max="200" bind:value={facteurCorrection} on:change={calculeCorrection} id="facteurCorrection">
-        <span>mg/dl/U</span>
-    </span>
+    <Setting
+        name="Facteur de correction"
+        min="1"
+        max="200"
+        bind:value={facteurCorrection}
+        unit="mg/dl/U"
+    />
 
     <span>
         <label for="objectif">Objectif glycémique</label>
-        <input type="number" min="80" max="200" bind:value={objectif} on:change={calculeCorrection} id ="objectif">
+        <input type="number" min="80" max="200" bind:value={objectif} id ="objectif">
         <span>mg/dl</span>
     </span>
 
     <span>
         <label for="rapport">Rapport glucides / insuline</label>
-        <input type="number" min="1" max="100" bind:value={rapportInsulineGlucides} on:change={calculeRepas} id="rapport">
+        <input type="number" min="1" max="100" bind:value={rapportInsulineGlucides} id="rapport">
         <span>g/U</span>
     </span>
 </div>
---
+++ src/Setting.svelte
@@ -0,0 +1,14 @@
+<script>
+    export let name = "Setting";
+    export let min = 1;
+    export let max = 200;
+    export let value;
+    export let handleChange = () => 0;
+    export let unit = "";
+</script>
+
+<span>
+    <label for="setting">{name}</label>
+    <input type="number" min={min} max={max} bind:value={value} on:change={handleChange} id="setting">
+    <span>{unit}</span>
+</span>
